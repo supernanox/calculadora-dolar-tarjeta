a1e070e04cf9e4ac8d8fc6e3558793508df40e9a
supernanox <supernanox@gmail.com>
2024-01-06 10:53:30 -0300

    Se actualiza script obteniendo el valor del dólar tarjeta

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
-function dolarTarjeta(){
-    fetch
+let cantidad = 5;
+let username = "Mariano";
+
+
+
+function consultarDolar(cant){
+
+    paraMult = cant;
+    
+    fetch("https://dolarapi.com/v1/dolares/tarjeta")
+  .then(response => response.json())
+  .then(data => {
+    console.log("El total a pagar es: " + (data.venta * paraMult))
+  }
+  );
+    
+
 }
+
+function hola(nombre){
+    console.log("Hola, " + nombre);
+}
+
+console.log(consultarDolar(cantidad));
+hola(username);
